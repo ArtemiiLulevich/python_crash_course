8ad4d2afd99f419eb817590ec6e28f59da9a771d
ArtemiiLulevich <artemii.lulevich@gmail.com>
2019-09-03 21:10:41 +0300

    New part of the book exercise.

---
+++ Lists/3-8. Countries(sort(), sorted()).py
@@ -0,0 +1,27 @@
+countries_list = ['Japan', 'Brazil', 'USA', 'Great Britain', 'German', 'France']
+
+print("List: " + str(countries_list))
+
+print("Sorted list: " + str(sorted(countries_list)))
+
+print("Sorted and reversed list: " + str(sorted(countries_list, reverse=True)))
+
+countries_list.reverse()
+
+print("Reversed list: " + str(countries_list))
+
+countries_list.reverse()
+
+print("Reversed back list: " + str(countries_list))
+
+countries_list.sort()
+
+print("Sorted: " + str(countries_list))
+
+countries_list.sort(reverse=True)
+
+print("Reversed list: " + str(countries_list))
+
+print("List length: " + str(len(countries_list)))
+
+# print(countries_list[len(countries_list)+1])
---
+++ Lists/4 - Work with lists(cycles).py
@@ -0,0 +1,6 @@
+animals = ['bear', 'wolf', 'fox']
+
+for animal in animals:
+    print('{} lives in a forest.'.format(animal.title()))
+
+print("Any of this animals lives in a forest.")
