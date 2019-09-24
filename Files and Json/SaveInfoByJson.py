90415c8d8316e85a486b5758c7d05128df59388a
ArtemiiLulevich <artemii.lulevich@gmail.com>
2019-09-24 21:30:51 +0300

    Chapter 10 finished.

--- Files and Json/SaveInfoByJson.py
+++ Files and Json/SaveInfoByJson.py
@@ -16,13 +16,65 @@ import json
 
 # 10-11
 
-favour_number = input('Enter your favourite number: ')
+# favour_number = input('Enter your favourite number: ')
+#
+# filename = 'favourite_number.json'
+# with open(filename, 'w') as f_obj:
+#     json.dump(favour_number, f_obj)
+#
+# with open(filename) as f_obj:
+#     number = json.load(f_obj)
+#
+# print("I know you favourite number! It's {}".format(number))
 
-filename = 'favourite_number.json'
-with open(filename, 'w') as f_obj:
-    json.dump(favour_number, f_obj)
+# 10-12
 
-with open(filename) as f_obj:
-    number = json.load(f_obj)
+# filename = 'favourite_number.json'
+# try:
+#     with open(filename) as f_obj:
+#         number = json.load(f_obj)
+# except FileNotFoundError:
+#     favour_number = input('Enter your favourite number: ')
+#     with open(filename, 'w') as f_obj:
+#         json.dump(favour_number, f_obj)
+# else:
+#     print("I know you favourite number! It's {}".format(number))
 
-print("I know you favourite number! It's {}".format(number))
+# 10-13
+
+
+def get_stored_username():
+    """Gets stored username if exists."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """Gets new username."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    print("We'll remember you when you come back, " + username + "!")
+
+
+def greet_user():
+    """Greets user by name."""
+    username = get_stored_username()
+    if username:
+        flag = input("Are you {}?Y/N: ".format(username))
+        if flag.lower() == 'y':
+            print("Welcome back, " + username + "!")
+        else:
+            get_new_username()
+    else:
+        get_new_username()
+
+
+greet_user()
